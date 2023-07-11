eed2bb14e74137a979692c646781f9c2f764a48e
Vander <vander@Vanders-MacBook-Pro.local>
2023-07-11 16:57:20 -0700

    added delete and edit

--- server/controllers/wineController.js
+++ server/controllers/wineController.js
@@ -2,25 +2,25 @@ const db = require('../models/wineModels');
 const wineController = {};
 
 wineController.getWines = async (req, res, next) => {
-    console.log('req body for getWines',req.body);
-    try{
-        if(!req.body){
+    console.log('req body for getWines', req.body);
+    try {
+        if (!req.body) {
             const result = await db.query(
                 `SELECT *
                 FROM wines`
-                );
-                res.locals.allWines = result.rows;
+            );
+            res.locals.allWines = result.rows;
             next();
-        }else{
+        } else {
             const result = await db.query(
                 `SELECT *
                 FROM wines
                 ORDER BY  ${req.body.sort} ${req.body.order}`
-                );
-                res.locals.allWines = result.rows;
+            );
+            res.locals.allWines = result.rows;
             next();
         }
-    }catch (error){
+    } catch (error) {
         next({
             log: 'Error in wineController.getWines',
             status: 500,
@@ -32,22 +32,43 @@ wineController.getWines = async (req, res, next) => {
 };
 wineController.addWine = (req, res, next) => {
     console.log('ADDING wine controler hit');
-    const {name, alcohol_percent, region, score, notes, date} = req.body;
+    const { name, alcohol_percent, region, score, notes, date } = req.body;
     const queryObj = {
-        text : 'INSERT INTO wines(name, alcohol_percent, region, score, notes, date) VALUES($1, $2, $3, $4, $5, $6) RETURNING *',
-        values : [name, alcohol_percent, region, score, notes, date]
+        text: 'INSERT INTO wines(name, alcohol_percent, region, score, notes, date) VALUES($1, $2, $3, $4, $5, $6) RETURNING *',
+        values: [name, alcohol_percent, region, score, notes, date]
     };
     db.query(queryObj).then(data => {
         res.locals.newWine = data.rows[0];
         next();
-    }).catch(err =>{
+    }).catch(err => {
         res.status(500);
         next(err);
     })
 };
 
-wineController.updateWine = (req, res, next) => {
-    
+wineController.updateWine = async (req, res, next) => {
+    console.log('UPDATING wine controler hit');
+    const result = await db.query(
+        `UPDATE wines
+        SET name = '${req.body.name}', region = '${req.body.region}'
+        WHERE wine_id = ${req.body.wine_id}
+        RETURNING *`
+    );
+    console.log('RESULT', result);
+    res.locals.updatedWines = result.rows;
+    next();
+}
+
+wineController.deleteWine = async (req, res, next) => {
+    console.log('DELETING wine hit');
+    const result = await db.query(
+        `DELETE FROM wines
+        WHERE wine_id = ${req.body.wine_id}
+        RETURNING *`
+    );
+    console.log('RESULT', result);
+    res.locals.deletedWine = result.rows;
+    next();
 }
 
 module.exports = wineController;
