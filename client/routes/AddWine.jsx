f953c4000754e476cfe43d9be511831ffe9e628c
staceyjhlee <staceyjlee18@gmail.com>
2023-07-11 17:22:16 -0400

    client-post

--- client/routes/AddWine.jsx
+++ client/routes/AddWine.jsx
@@ -1,41 +1,95 @@
 import React from "react";
-
+import { useState } from "react";
 export default function AddWine() {
-  const handleSubmit = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    alcohol_percent: "",
+    region: "",
+    score: "",
+    notes: "",
+    date: "",
+  });
+  const handleSubmit = (e) => {
     e.preventDefault();
     const postData = async () => {
+      console.log('postin')
       try {
+        console.log('postin2')
         const response = await fetch("/api/winelist", {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify(formData),
         });
       } catch (err) {
+
         console.log("error");
       }
     };
+    postData();
   };
 
   return (
     <div className="routecontainers" onSubmit={handleSubmit}>
       <form className="wineform">
         <label>
-          WINE: <input name="username" type="text" />
+          WINE:{" "}
+          <input
+            onChange={(e) => {
+              setFormData({ ...formData, name: e.target.value });
+            }}
+            name="name"
+            type="text"
+          />
         </label>{" "}
         <label>
-          ALCOHOL %: <input name="username" type="text" />
+          ALCOHOL %:{" "}
+          <input
+            onChange={(e) => {
+              setFormData({ ...formData, alcohol_percent: e.target.value });
+            }}
+            name="alcohol_percent"
+            type="text"
+          />
         </label>{" "}
         <label>
-          REGION: <input name="username" type="text" />
+          REGION:{" "}
+          <input
+            onChange={(e) => {
+              setFormData({ ...formData, region: e.target.value });
+            }}
+            name="region"
+            type="text"
+          />
         </label>{" "}
         <label>
-          SCORE: <input name="username" type="text" />
+          SCORE:{" "}
+          <input
+            onChange={(e) => {
+              setFormData({ ...formData, score: e.target.value });
+            }}
+            name="score"
+            type="text"
+          />
         </label>{" "}
         <label>
-          NOTES: <input name="username" type="text" />
+          NOTES:{" "}
+          <input
+            onChange={(e) => {
+              setFormData({ ...formData, notes: e.target.value });
+            }}
+            name="notes"
+            type="text"
+          />
         </label>{" "}
         <label>
-          DATE: <input name="username" type="text" />
+          DATE:{" "}
+          <input
+            onChange={(e) => {
+              setFormData({ ...formData, date: e.target.value });
+            }}
+            name="date"
+            type="text"
+          />
         </label>{" "}
         <br />
         <button type="submit">Add Wine</button>
