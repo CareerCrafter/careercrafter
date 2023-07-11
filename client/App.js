ea2171e949157b7f8b5594a9c4e5a9c76f555a5b
esa <esa>
2023-07-11 13:49:57 -0700

    working

--- client/App.js
+++ client/App.js
@@ -1,12 +1,11 @@
 import React from "react";
-import { Auth } from '@supabase/auth-ui-react'
-import { ThemeSupa } from '@supabase/auth-ui-shared'
+
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Root from "./routes/Root.jsx";
 import Login from "./routes/Login.jsx";
 import Wine from "./routes/Wine.jsx";
 import Vineyard from "./routes/Vineyard.jsx";
-import AuthConsumer from "./AuthProvider.jsx";
+
 
 const router = createBrowserRouter([
   {
@@ -29,38 +28,7 @@ const router = createBrowserRouter([
 
 export default function App() {
   
-  const { session, supabase } = AuthConsumer();  
   
-  
-  if (!session) {
-    console.log('no session')
-    return (
-      <div
-        styles={{
-          width: '500px',
-          height: '500px'
-        }}
-      >
-        <Auth 
-          supabaseClient={supabase}
-          appearance={{
-          theme: ThemeSupa,
-          variables: {
-          default: {
-            colors: {
-              brand: 'black',
-              brandAccent: 'green',
-              },
-            },
-          },
-        }}
-        providers={['google', 'facebook', 'twitter']}
-      />
-      </div>
-    )
-  }
-  else {
-    console.log('session');
     return (
       
       <RouterProvider 
@@ -69,4 +37,4 @@ export default function App() {
     
     );
   }
-}
+
