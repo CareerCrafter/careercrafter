8545a6f7614e1c490894f458e45fc6de73428ad7
esa <esa>
2023-07-10 17:33:07 -0700

    update webpack, server, routes

---
+++ client/routes/Login.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import { useNavigate, useLocation } from "react-router-dom";
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '60%',
+  transform: 'translate(-50%, -50%)',
+  width: '40rem',
+  bgcolor: 'background.paper',
+  border: '1px solid #000',
+  borderRadius: '15px',
+  boxShadow: 24,
+  p: 4,
+};
+
+export default function Login() {
+
+  const navigate = useNavigate();
+  let location = useLocation();
+  let from = location?.pathname || "/";
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    let formData = new FormData(event.currentTarget);
+    let username = formData.get("username")
+    console.log(username);
+    console.log('location', location, 'from', from)
+    navigate(from);
+  }
+
+  return (
+    <div className="detail">
+      <Box sx={style}>
+        
+        <Typography id="modal-modal-title" variant="h6" component="h2">
+          Only authenticated users can edit data.
+        </Typography>
+        <Typography id="modal-modal-description" sx={{ mt: 2 }}>         
+        </Typography>
+        
+        <form onSubmit={handleSubmit}>
+          
+          <label>
+            Username: <input  name="username" type="text" />
+          </label>{" "}
+          
+          <br/>
+          
+          <label>
+            Password: <input  name="username" type="text" />
+          </label>{" "}
+          
+          <br/>
+          
+          <Button type="submit"> 
+            Login 
+          </Button>
+
+        </form>
+      
+      </Box>
+    </div>
+  )
+}
